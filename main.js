fd773fd30ec2db00c5740919a8a4f96274257e2d
Sara Raquel Madeira Pina <sararmpina@gmail.com>
2026-01-16 20:04:36 +0000

    tudo update e limpo

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// Exer1: Cria uma interface chamada Tarefa que defina a estrutura de um objeto de tarefa
-// Exerc2: Cria uma classe TarefaClass que implemente a interface Tarefa.
 var Tarefa = /** @class */ (function () {
     function Tarefa(id, titulo) {
         this.concluida = false;
@@ -22,133 +20,49 @@ var Tarefa = /** @class */ (function () {
     };
     return Tarefa;
 }());
-// Exerc3: Cria um array chamado listaTarefas que armazene vários objetos do tipo Tarefa.
 var irCompras = new Tarefa(1, "ir às compras");
 var estudar = new Tarefa(2, "estudar programação");
-irCompras.marcarConcluida();
 var listaTarefas = [irCompras, estudar];
-console.log("o array é", listaTarefas);
-console.log("olá");
-addTaskstoHtml();
-function addTaskstoHtml() {
+var inputTarefa = document.getElementById("addTarefa");
+renderTasks();
+createBtnAddTask();
+function createSingleTask(task) {
+    var elemLista = document.createElement("li");
+    elemLista.textContent = task.titulo;
+    if (task.concluida == true) {
+        elemLista.classList.add("riscarTarefa");
+        // let dataConclusao = listaTarefas[i].marcarConcluida(); 
+        // elemLista.textContent = listaTarefas[i].titulo + " " + String(dataConclusao)
+    }
+    elemLista.appendChild(createBtnRemove(task));
+    elemLista.appendChild(createBtnEdit(elemLista, task));
+    elemLista.appendChild(createBtnToggleCheck(task));
+    return elemLista;
+}
+function renderTasks() {
     atualizarBadge();
     var lista = document.querySelector("#taskList");
     lista.innerHTML = "";
-    var _loop_1 = function (i) {
-        var elemLista = document.createElement("li");
-        elemLista.textContent = listaTarefas[i].titulo;
-        if (listaTarefas[i].concluida == true) {
-            elemLista.classList.add("riscarTarefa");
-            // let dataConclusao = listaTarefas[i].marcarConcluida(); 
-            // elemLista.textContent = listaTarefas[i].titulo + " " + String(dataConclusao)
-        }
-        var btnRemover = document.createElement("button");
-        btnRemover.textContent = "Remover";
-        elemLista.appendChild(btnRemover);
-        var btnEdicao = document.createElement("button");
-        btnEdicao.textContent = "Editar";
-        var btnCompleto = document.createElement("button");
-        btnCompleto.textContent = "Toggle Check";
-        elemLista.appendChild(btnEdicao);
-        elemLista.appendChild(btnCompleto);
-        lista.appendChild(elemLista);
-        // btnRemover.addEventListener("click", () => 
-        //     elemLista.remove()); 
-        btnCompleto.addEventListener("click", function () { return identifyTaskChecked(listaTarefas[i].id); });
-        btnRemover.addEventListener("click", function () { return removerTarefa(listaTarefas[i].id); });
-        // btnEdicao.addEventListener("click", () => editarTarefa (listaTarefas[i].id)); 
-        // btnEdicao.addEventListener("click", () => adicionarInputEdicao (elemLista, listaTarefas[i].id)); 
-        btnEdicao.addEventListener("click", function () { return addInput(elemLista, listaTarefas[i].titulo, listaTarefas[i].id); });
-    };
     for (var i = 0; i < listaTarefas.length; i++) {
-        _loop_1(i);
+        lista.appendChild(createSingleTask(listaTarefas[i]));
     }
 }
-function removerTarefa(identificador) {
+function createBtnRemove(task) {
+    var btnRemover = document.createElement("button");
+    btnRemover.textContent = "Remove";
+    btnRemover.addEventListener("click", function () { return removeTask(task.id); });
+    return btnRemover;
+}
+function removeTask(identificador) {
     var listaSemTarefa = listaTarefas.filter(function (elemento) { return elemento.id != identificador; });
     listaTarefas = listaSemTarefa;
-    console.log(" a minha lista sem a tarefa eliminada é", listaTarefas);
-    addTaskstoHtml();
+    renderTasks();
 }
-function addInput(tagLi, title, identificador) {
-    var inputNovaTarefa = document.createElement("input");
-    inputNovaTarefa.setAttribute("placeholder", title);
-    var btnConclusao = document.createElement("button");
-    btnConclusao.textContent = "Atualizar";
-    var btnCancelar = document.createElement("button");
-    btnCancelar.textContent = "Cancelar";
-    tagLi.innerHTML = "";
-    tagLi.appendChild(inputNovaTarefa);
-    tagLi.appendChild(btnConclusao);
-    tagLi.appendChild(btnCancelar);
-    btnCancelar.addEventListener("click", function () { return addTaskstoHtml(); });
-    btnConclusao.addEventListener("click", function () { return changeTask(inputNovaTarefa, identificador); });
-}
-function changeTask(tagInput, numId) {
-    var novoTitulo = tagInput.value;
-    var arrayTemporario = listaTarefas.filter(function (objeto) { return objeto.id == numId; });
-    console.log(arrayTemporario);
-    arrayTemporario[0].titulo = novoTitulo;
-    addTaskstoHtml();
-}
-// function editarTarefa (identificador: number) {
-//     let elemEditado = listaTarefas.filter(elemento => elemento.id == identificador);
-//     console.log(elemEditado); 
-// }
-// function adicionarInputEdicao (elemento: HTMLLIElement, identificador: number) {
-//     console.log("O elemento HTML é", elemento); 
-//     let tarefaAlterada = document.createElement("input");
-//     tarefaAlterada.setAttribute("placeholder", "Edite a sua tarefa aqui"); 
-//     let enter = document.createElement("br"); 
-//     let btnConcluida = document.createElement("button"); 
-//     btnConcluida.textContent = "Concluir"; 
-//     elemento.appendChild(enter); 
-//     elemento.appendChild(tarefaAlterada); 
-//     elemento.appendChild(btnConcluida); 
-//     btnConcluida.addEventListener("click", () => alterarTarefaArray (identificador, tarefaAlterada))
-// }
-// function alterarTarefaArray (codigo: number, tarefa: HTMLInputElement) {
-//     let arrayPrecisaEdicao = listaTarefas.filter(elemento => elemento.id == codigo);
-//     console.log("O objeto do tipo Tarefa é", arrayPrecisaEdicao); 
-//     let objetoTarefa = arrayPrecisaEdicao[0]
-//     console.log("O objeto do array é", objetoTarefa); 
-//     console.log("O input criado é", tarefa); 
-//     let novoTitulo = tarefa.value;
-//     console.log("O novotitulo tem que ser", novoTitulo)
-//     objetoTarefa.titulo = novoTitulo; 
-//     console.log("elemento c nova propriedade titulo e",objetoTarefa); 
-//     addTaskstoHtml(); 
-//     console.log(listaTarefas); 
-// }
-// Exerc4: Cria um campo de texto e um botão para adicionar novas tarefas ao array.
-var inputTarefa = document.getElementById("addTarefa");
-var btnAdd = document.getElementById("addTask");
-btnAdd.addEventListener("click", function () { return addToTaskList(); });
-function addToTaskList() {
-    var tarefaIntroduzida = inputTarefa.value;
-    var novaTarefa = new Tarefa(Date.now(), tarefaIntroduzida);
-    listaTarefas.push(novaTarefa);
-    console.log("esta é a lista atualizada", listaTarefas);
-    inputTarefa.value = "";
-    addTaskstoHtml();
-}
-// Exerc7: Adiciona um botão "Remover" ao lado de cada tarefa na lista.
-// Exerc8: Adiciona um botão "Editar" ao lado de cada tarefa na lista.
-//Exerc9
-function contarTarefasNaoCumpridas() {
-    var contador = 0;
-    for (var i = 0; i < listaTarefas.length; i++) {
-        if (listaTarefas[i].concluida == false) {
-            contador = contador + 1;
-        }
-    }
-    console.log("O contador é", contador);
-    return contador;
-}
-function atualizarBadge() {
-    var valorNovoDoBadge = contarTarefasNaoCumpridas();
-    var span = document.querySelector("#badge");
-    span.textContent = String(valorNovoDoBadge);
+function createBtnToggleCheck(task) {
+    var btnCompleto = document.createElement("button");
+    btnCompleto.textContent = "Toggle Check";
+    btnCompleto.addEventListener("click", function () { return identifyTaskChecked(task.id); });
+    return btnCompleto;
 }
 function identifyTaskChecked(numId) {
     var arrayTarefasConcluidas = listaTarefas.filter(function (tarefa) { return tarefa.id == numId; });
@@ -159,5 +73,60 @@ function identifyTaskChecked(numId) {
     else {
         tarefa.concluida = true;
     }
-    addTaskstoHtml();
+    renderTasks();
+}
+function createBtnEdit(li, task) {
+    var btnEdicao = document.createElement("button");
+    btnEdicao.textContent = "Edit";
+    btnEdicao.addEventListener("click", function () { return addInput(li, task.titulo, task.id); });
+    return btnEdicao;
+}
+function addInput(tagLi, title, identificador) {
+    var inputNovaTarefa = document.createElement("input");
+    inputNovaTarefa.setAttribute("placeholder", title);
+    var btnConclusao = document.createElement("button");
+    btnConclusao.textContent = "Update";
+    tagLi.innerHTML = "";
+    tagLi.appendChild(inputNovaTarefa);
+    tagLi.appendChild(btnConclusao);
+    tagLi.appendChild(createBtnCancel());
+    btnConclusao.addEventListener("click", function () { return changeTask(inputNovaTarefa, identificador); });
+}
+function createBtnCancel() {
+    var btnCancelar = document.createElement("button");
+    btnCancelar.textContent = "Cancel";
+    btnCancelar.addEventListener("click", function () { return renderTasks(); });
+    return btnCancelar;
+}
+function changeTask(tagInput, numId) {
+    var novoTitulo = tagInput.value;
+    var arrayTemporario = listaTarefas.filter(function (objeto) { return objeto.id == numId; });
+    console.log(arrayTemporario);
+    arrayTemporario[0].titulo = novoTitulo;
+    renderTasks();
+}
+function createBtnAddTask() {
+    var btnAdd = document.getElementById("addTask");
+    btnAdd.addEventListener("click", function () { return addToTaskList(); });
+}
+function addToTaskList() {
+    var tarefaIntroduzida = inputTarefa.value;
+    var novaTarefa = new Tarefa(Date.now(), tarefaIntroduzida);
+    listaTarefas.push(novaTarefa);
+    inputTarefa.value = "";
+    renderTasks();
+}
+function contarTarefasNaoCumpridas() {
+    var contador = 0;
+    for (var i = 0; i < listaTarefas.length; i++) {
+        if (listaTarefas[i].concluida == false) {
+            contador = contador + 1;
+        }
+    }
+    return contador;
+}
+function atualizarBadge() {
+    var valorNovoDoBadge = contarTarefasNaoCumpridas();
+    var span = document.querySelector("#badge");
+    span.textContent = String(valorNovoDoBadge);
 }
--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 
 
-
 interface TarefaInterface {
     id: number;
     titulo: string; 
@@ -41,185 +40,69 @@ class Tarefa implements TarefaInterface {
 let irCompras = new Tarefa (1, "ir às compras")
 let estudar = new Tarefa (2, "estudar programação")
 
-irCompras.marcarConcluida();  
 
 let listaTarefas: Tarefa[] = [irCompras, estudar]; 
+let inputTarefa = document.getElementById("addTarefa") as HTMLInputElement; 
 
-console.log("o array é",listaTarefas)
-console.log("olá")
+renderTasks(); 
+createBtnAddTask(); 
 
-addTaskstoHtml(); 
+function createSingleTask(task: Tarefa) {
 
+    let elemLista = document.createElement("li") as HTMLLIElement; 
+    elemLista.textContent = task.titulo; 
 
-function addTaskstoHtml () {
-    atualizarBadge();
-
-    let lista = document.querySelector("#taskList") as HTMLUListElement; 
-    lista.innerHTML = ""; 
-    for (let i=0;  i<listaTarefas.length; i++) {
-        let elemLista = document.createElement("li") as HTMLLIElement; 
-        elemLista.textContent = listaTarefas[i].titulo; 
-        if (listaTarefas[i].concluida == true) {
+        if (task.concluida == true) {
             elemLista.classList.add("riscarTarefa"); 
             // let dataConclusao = listaTarefas[i].marcarConcluida(); 
             // elemLista.textContent = listaTarefas[i].titulo + " " + String(dataConclusao)
         } 
 
-        let btnRemover = document.createElement("button") as HTMLButtonElement; 
-        btnRemover.textContent = "Remover"; 
-        elemLista.appendChild(btnRemover); 
+    elemLista.appendChild(createBtnRemove(task)); 
+    elemLista.appendChild(createBtnEdit(elemLista, task)); 
+    elemLista.appendChild(createBtnToggleCheck(task)); 
 
-        let btnEdicao = document.createElement("button") as HTMLButtonElement;
-        btnEdicao.textContent = "Editar"; 
-
-        let btnCompleto = document.createElement("button") as HTMLButtonElement;
-        btnCompleto.textContent = "Toggle Check"
-
-        elemLista.appendChild(btnEdicao); 
-        elemLista.appendChild(btnCompleto); 
-
-        lista.appendChild(elemLista); 
-
-        // btnRemover.addEventListener("click", () => 
-        //     elemLista.remove()); 
-
-
-        btnCompleto.addEventListener("click", () => identifyTaskChecked(listaTarefas[i].id)); 
-
-        btnRemover.addEventListener("click", () => removerTarefa (listaTarefas[i].id));
-        
-        // btnEdicao.addEventListener("click", () => editarTarefa (listaTarefas[i].id)); 
-
-        // btnEdicao.addEventListener("click", () => adicionarInputEdicao (elemLista, listaTarefas[i].id)); 
-
-        btnEdicao.addEventListener("click", () => addInput (elemLista, listaTarefas[i].titulo, listaTarefas[i].id)); 
-}
+    return elemLista; 
 }
 
-function removerTarefa (identificador: number) {
+function renderTasks() {
+    atualizarBadge();
+
+    let lista = document.querySelector("#taskList") as HTMLUListElement; 
+
+    lista.innerHTML = ""; 
+
+    for (let i=0;  i<listaTarefas.length; i++) {
+        lista.appendChild(createSingleTask(listaTarefas[i]));   
+    }
+}
+
+function createBtnRemove(task: Tarefa) {
+
+    let btnRemover = document.createElement("button") as HTMLButtonElement; 
+    btnRemover.textContent = "Remove";
+
+    btnRemover.addEventListener("click", () => removeTask (task.id));
+
+    return btnRemover; 
+}
+
+function removeTask (identificador: number) {
     let listaSemTarefa = listaTarefas.filter(elemento => elemento.id != identificador)
     listaTarefas = listaSemTarefa;
-    console.log(" a minha lista sem a tarefa eliminada é",listaTarefas); 
-    addTaskstoHtml(); 
-}
-
-function addInput(tagLi: HTMLLIElement, title: string, identificador: number) {
-    let inputNovaTarefa = document.createElement("input") as HTMLInputElement; 
-    inputNovaTarefa.setAttribute("placeholder", title); 
-
-    let btnConclusao = document.createElement("button") as HTMLButtonElement; 
-    btnConclusao.textContent = "Atualizar"; 
-
-    let btnCancelar = document.createElement("button") as HTMLButtonElement; 
-    btnCancelar.textContent = "Cancelar"; 
-
-    tagLi.innerHTML = ""; 
-    tagLi.appendChild(inputNovaTarefa); 
-    tagLi.appendChild(btnConclusao);
-    tagLi.appendChild(btnCancelar); 
-
-
-    btnCancelar.addEventListener("click", () => addTaskstoHtml()); 
-
-    btnConclusao.addEventListener("click", () => changeTask (inputNovaTarefa, identificador)); 
-      
-}
-
-function changeTask (tagInput: HTMLInputElement, numId: number) {
-    let novoTitulo = tagInput.value; 
-
-    let arrayTemporario = listaTarefas.filter(objeto => objeto.id == numId)
-    console.log(arrayTemporario); 
-
-    arrayTemporario[0].titulo = novoTitulo;
-
-    addTaskstoHtml(); 
-}
-
-// function editarTarefa (identificador: number) {
-//     let elemEditado = listaTarefas.filter(elemento => elemento.id == identificador);
-//     console.log(elemEditado); 
-// }
-
-// function adicionarInputEdicao (elemento: HTMLLIElement, identificador: number) {
-//     console.log("O elemento HTML é", elemento); 
-//     let tarefaAlterada = document.createElement("input");
-//     tarefaAlterada.setAttribute("placeholder", "Edite a sua tarefa aqui"); 
-
-//     let enter = document.createElement("br"); 
-//     let btnConcluida = document.createElement("button"); 
-//     btnConcluida.textContent = "Concluir"; 
-
-//     elemento.appendChild(enter); 
-//     elemento.appendChild(tarefaAlterada); 
-//     elemento.appendChild(btnConcluida); 
-
-//     btnConcluida.addEventListener("click", () => alterarTarefaArray (identificador, tarefaAlterada))
-// }
-
-// function alterarTarefaArray (codigo: number, tarefa: HTMLInputElement) {
+   
     
-//     let arrayPrecisaEdicao = listaTarefas.filter(elemento => elemento.id == codigo);
-//     console.log("O objeto do tipo Tarefa é", arrayPrecisaEdicao); 
-
-//     let objetoTarefa = arrayPrecisaEdicao[0]
-
-//     console.log("O objeto do array é", objetoTarefa); 
-//     console.log("O input criado é", tarefa); 
-
-//     let novoTitulo = tarefa.value;
-//     console.log("O novotitulo tem que ser", novoTitulo)
-
-//     objetoTarefa.titulo = novoTitulo; 
-
-//     console.log("elemento c nova propriedade titulo e",objetoTarefa); 
-//     addTaskstoHtml(); 
-
-//     console.log(listaTarefas); 
-// }
-
-
-// Exerc4: Cria um campo de texto e um botão para adicionar novas tarefas ao array.
-
-let inputTarefa = document.getElementById("addTarefa") as HTMLInputElement; 
-
-let btnAdd = document.getElementById("addTask") as HTMLButtonElement; 
-
-btnAdd.addEventListener("click", () => addToTaskList())
-
-
-function addToTaskList () {
-    let tarefaIntroduzida: string = inputTarefa.value; 
-    let novaTarefa = new Tarefa (Date.now(), tarefaIntroduzida)
-    listaTarefas.push(novaTarefa); 
-    console.log("esta é a lista atualizada", listaTarefas); 
-    inputTarefa.value = ""; 
-    addTaskstoHtml(); 
+    renderTasks(); 
 }
 
-// Exerc7: Adiciona um botão "Remover" ao lado de cada tarefa na lista.
 
-// Exerc8: Adiciona um botão "Editar" ao lado de cada tarefa na lista.
+function createBtnToggleCheck(task: Tarefa) {
+    let btnCompleto = document.createElement("button") as HTMLButtonElement;
+    btnCompleto.textContent = "Toggle Check"
 
-//Exerc9
+    btnCompleto.addEventListener("click", () => identifyTaskChecked(task.id)); 
 
-function contarTarefasNaoCumpridas () {
-    let contador = 0; 
-    for (let i =0; i<listaTarefas.length; i++) {
-        if (listaTarefas[i].concluida == false) {
-            contador = contador + 1; 
-        }
-    }
-    console.log("O contador é", contador)
-    return contador;
-}
-
-function atualizarBadge() {
-    let valorNovoDoBadge: number = contarTarefasNaoCumpridas(); 
-
-    let span = document.querySelector("#badge") as HTMLSpanElement; 
-
-    span.textContent = String(valorNovoDoBadge); 
+    return btnCompleto; 
 }
 
 
@@ -233,6 +116,99 @@ function identifyTaskChecked(numId: number) {
   } else {
     tarefa.concluida = true; 
   }
-  addTaskstoHtml(); 
+  renderTasks(); 
+}
+
+function createBtnEdit (li: HTMLLIElement, task: Tarefa) {
+
+    let btnEdicao = document.createElement("button") as HTMLButtonElement;
+    btnEdicao.textContent = "Edit"; 
+
+
+    btnEdicao.addEventListener("click", () => addInput (li, task.titulo, task.id)); 
+
+    return btnEdicao; 
+}
+
+
+function addInput(tagLi: HTMLLIElement, title: string, identificador: number) {
+
+    let inputNovaTarefa = document.createElement("input") as HTMLInputElement; 
+    inputNovaTarefa.setAttribute("placeholder", title); 
+
+    let btnConclusao = document.createElement("button") as HTMLButtonElement; 
+    btnConclusao.textContent = "Update"; 
+
+   
+
+    tagLi.innerHTML = ""; 
+    tagLi.appendChild(inputNovaTarefa); 
+    tagLi.appendChild(btnConclusao);
+    tagLi.appendChild(createBtnCancel()); 
+
+
+    btnConclusao.addEventListener("click", () => changeTask (inputNovaTarefa, identificador)); 
+      
+}
+
+function createBtnCancel () {
+
+    let btnCancelar = document.createElement("button") as HTMLButtonElement; 
+
+    btnCancelar.textContent = "Cancel"; 
+
+     btnCancelar.addEventListener("click", () => renderTasks()); 
+
+    return btnCancelar; 
+}
+
+
+function changeTask (tagInput: HTMLInputElement, numId: number) {
+    let novoTitulo = tagInput.value; 
+
+    let arrayTemporario = listaTarefas.filter(objeto => objeto.id == numId)
+    console.log(arrayTemporario); 
+
+    arrayTemporario[0].titulo = novoTitulo;
+
+    renderTasks(); 
+}
+
+function createBtnAddTask() {
+    let btnAdd = document.getElementById("addTask") as HTMLButtonElement; 
+
+    btnAdd.addEventListener("click", () => addToTaskList())
+}
+
+
+function addToTaskList () {
+    let tarefaIntroduzida: string = inputTarefa.value; 
+    let novaTarefa = new Tarefa (Date.now(), tarefaIntroduzida)
+
+    listaTarefas.push(novaTarefa); 
+    
+    inputTarefa.value = ""; 
+
+    renderTasks(); 
+}
+
+
+function contarTarefasNaoCumpridas () {
+    let contador = 0; 
+    for (let i =0; i<listaTarefas.length; i++) {
+        if (listaTarefas[i].concluida == false) {
+            contador = contador + 1; 
+        }
+    }
+    
+    return contador;
+}
+
+function atualizarBadge() {
+    let valorNovoDoBadge: number = contarTarefasNaoCumpridas(); 
+
+    let span = document.querySelector("#badge") as HTMLSpanElement; 
+
+    span.textContent = String(valorNovoDoBadge); 
 }
 
